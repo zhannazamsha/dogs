3566c9c3d9d31c32f388722bf8a1764e890631d4
Zhanna_Zamsha <z.zhanna@gmail.com>
2022-10-04 18:08:50 +0300

    init

---
+++ src/main/kotlin/com/learn/dogs/DogsApplication.kt
@@ -0,0 +1,11 @@
+package com.learn.dogs
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DogsApplication
+
+fun main(args: Array<String>) {
+    runApplication<DogsApplication>(*args)
+}
